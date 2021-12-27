8cd3a74f2dfdbc609a5012957c553c126df5efe5
lifujun <lifujun@alibaba-inc.com>
2021-12-27 11:48:25 +0800

    update delete pod script
    Signed-off-by: lifujun <lifujun@alibaba-inc.com>

--- deletePod.sh
+++ deletePod.sh
@@ -1,3 +1,12 @@
-pod trunk delete AliPlayerPartSDK_iOS 5.4.1.2
-pod trunk delete AliPlayerSDK_iOS 5.4.1.2
-pod trunk delete AliPlayerSDK_iOS_ARTC 5.4.1.2
+export targetVersion=$1
+
+
+if [ ! "${targetVersion}" ] 
+    then
+    echo "缺少要删除的pod版本号"
+    exit
+fi
+
+pod trunk delete AliPlayerPartSDK_iOS ${targetVersion}
+pod trunk delete AliPlayerSDK_iOS ${targetVersion}
+pod trunk delete AliPlayerSDK_iOS_ARTC ${targetVersion}
